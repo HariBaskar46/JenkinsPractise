181d03f2b049116a17769f6c6a2eaa491a4aa34b
HariBaskar46 <mharibaskar7@gmail.com>
2026-05-19 12:51:48 +0530

    Create sample.java

---
+++ sample.java
@@ -0,0 +1,12 @@
+public class hellobuddy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello buddy");
+		System.out.println("hello baskie");
+    	System.out.println("hello nish");
+		System.out.println("hello push");
+
+	}
+
+}
